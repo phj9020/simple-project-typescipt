cad8ca39591a28b0aa3ebbfa33d22d710c2e8071
Han jin Park <phj9020@hotmail.com>
2022-10-29 09:35:10 +0900

    asnyc await format

--- src/service/youtube.ts
+++ src/service/youtube.ts
@@ -11,20 +11,20 @@ export default class Youtube implements IYoutube {
         this.key = key;
         
     }
-    getMostPopular() {
-        return fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&key=${this.key}`)
-            .then(response => response.json())
-            .then(result => result.items);
+    async getMostPopular() {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=50&key=${this.key}`);
+        const result = await response.json();
+        return result.items;
     }
     // VideoEntity의 id 형태가 popular api에서는 string 이지만 search api로 오는 id 는 object인 문제 => id 값을 item.id.videoId 로 덮어줌   
-    getSearch(arg: string) {
-        return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${arg}&type=video&key=${this.key}`)
-            .then(response => response.json())
-            .then(result => result.items.map((item: VideoEntity) => ({
-                ...item,
-                id: item.id.videoId,
-            })))
-            .then(items => items);
+    async getSearch(arg: string) {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${arg}&type=video&key=${this.key}`);
+        const result = await response.json();
+        const items = result.items.map((item: VideoEntity) => ({
+            ...item,
+            id: item.id.videoId,
+        }));
+        return items;
             
     }
 };
